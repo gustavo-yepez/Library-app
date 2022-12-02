5277649e9e842a7e3800085072b6c382cd98785a
gustavo yepez <gustavoyepez96@gmail.com>
2022-12-02 15:55:00 -0500

    tried to fix scripts

---
+++ build/precache-manifest.d9394df2596cd83225ed5e3ce3d45a26.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "85c8e490b7d78d150de8",
+    "url": "/static/css/main.7615939b.chunk.css"
+  },
+  {
+    "revision": "85c8e490b7d78d150de8",
+    "url": "/static/js/main.85c8e490.chunk.js"
+  },
+  {
+    "revision": "f28fd7addbb9390951eb",
+    "url": "/static/js/1.f28fd7ad.chunk.js"
+  },
+  {
+    "revision": "b8ab3c983beb7e977587",
+    "url": "/static/js/2.b8ab3c98.chunk.js"
+  },
+  {
+    "revision": "d164fe6a1fbd988e1378",
+    "url": "/static/js/runtime~main.d164fe6a.js"
+  },
+  {
+    "revision": "fcaba50a9bcbbb35b642dd082d427be9",
+    "url": "/static/media/Library.fcaba50a.svg"
+  },
+  {
+    "revision": "bb4a1a2bd361b535645749afeb500e86",
+    "url": "/static/media/Undraw_Books.bb4a1a2b.svg"
+  },
+  {
+    "revision": "c11647cafa4f63cfd2a182ee371a2dd7",
+    "url": "/static/media/empty_cart.c11647ca.svg"
+  },
+  {
+    "revision": "ea454e2f2a5c89357f3dead974371971",
+    "url": "/static/media/wave.ea454e2f.svg"
+  },
+  {
+    "revision": "e3f08a85c4d8abd2de581c3df9feb03e",
+    "url": "/index.html"
+  }
+];
